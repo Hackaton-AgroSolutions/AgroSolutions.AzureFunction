10639c20ae0fd4e15f1f6869847a6b3ef03ae05e
MarioGuilherme <marioguilhermedev@gmail.com>
2026-02-27 12:58:50 -0300

    fix

--- AgroSolutions.Alert.Infrastructure/DomainServices/AlertsDomainService.cs
+++ AgroSolutions.Alert.Infrastructure/DomainServices/AlertsDomainService.cs
@@ -62,32 +62,22 @@ public class AlertsDomainService(IInfluxDbService influxDb) : IAlertsDomainServi
         return true;
     }
 
-    // Rule number 2: If the average temperature over the last 12 hours is between 22°C and 30°C and the average air humidity over the last 12 hours is greater than 80% → "Plague Risk"
+    // Rule number 2: If soil ph is above 8 and air temperature is above 40ºC for 10 hours → "Plague Risk"
     private async Task<bool> CheckPlagueRiskAsync(ReceivedSensorDataEvent receivedSensorDataEvent)
     {
-        IEnumerable<FluxTable> tables = await _influxDb.QueryAsync("temp = " +
-        "    from(bucket: \"main-bucket\")" +
-        "        |> range(start: -12h)"+
-        $"        |> filter(fn: (r) => r.sensor_client_id == \"{receivedSensorDataEvent.SensorClientId}\" and r._field == \"air_temperature_c\")" +
-        "        |> mean()"+
-        "        |> rename(columns: {_value: \"temp_value\"})" +
+        IEnumerable<FluxTable> tables = tables = await _influxDb.QueryAsync("from(bucket:\"main-bucket\")" +
+        "    |> range(start: -10h)" +
+        "    |> filter(fn: (r) => r._measurement == \"agro_sensors\")" +
+        $"    |> filter(fn: (r) => r.sensor_client_id == \"{receivedSensorDataEvent.SensorClientId}\")" +
+        "    |> filter(fn: (r) =>" +
+        "        r._field == \"soil_ph\" or" +
+        "        r._field == \"air_temperature_c\")" +
+        "    |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\")" +
+        "    |> filter(fn: (r) =>" +
+        "        r.soil_ph > 8 and" +
+        "        r.air_temperature_c >= 40)");
 
-        "humidity = "+
-        "    from(bucket: \"main-bucket\")" +
-        "        |> range(start: -12h)"+
-        $"        |> filter(fn: (r) => r.sensor_client_id == \"{receivedSensorDataEvent.SensorClientId}\" and r._field == \"air_humidity_percent\")" +
-        "        |> mean()"+
-        "        |> rename(columns: {_value: \"humidity_value\"})"+
-
-        "join(tables: {temp: temp, humidity: humidity}, on: [\"_start\",\"_stop\"])"+
-        "    |> map(fn: (r) => ({"+
-        "        _value: r.temp_value >= 22.0 and "+
-        "                r.temp_value <= 30.0 and "+
-        "                r.humidity_value > 80.0"+
-        "    }))"
-        );
-
-        if (!tables.SelectMany(t => t.Records).Any())
+        if (tables.SelectMany(t => t.Records).ToList().Count == 0)
             return false;
 
         Log.Warning("The field with ID {FieldId} and the sensor with ID {SensorClientId} present a pest.", receivedSensorDataEvent.FieldId, receivedSensorDataEvent.SensorClientId);
